e1dce71560ef78cadf2b263e210993aff7cc9c4e
Abhilash Pani <abhilashpani651@gmail.com>
2020-01-27 13:29:36 +0530

    Add decoding and minor comments

--- flipkart_checker.py
+++ flipkart_checker.py
@@ -1,11 +1,11 @@
-import urllib2
+import urllib.request as u
 import re
 import subprocess
 # URL to the product
-flipkart_url = '<ENTER THE URL HERE>'
-flipkart_fetch = urllib2.urlopen(flipkart_url).read()
+flipkart_url = '<ADD HOSTNAME TO PRODUCT'
+flipkart_fetch = u.urlopen('http://' + flipkart_url).read().decode('utf-8')
 
-product = re.findall('This item is currently out of stock', flipkart_fetch);
+product = re.findall('This item is currently out of stock', flipkart_fetch)
 
 if len(product) == 0: 
 	message = "The Product is back in stock at Flipkart "
